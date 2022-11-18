3ad6aa4c0951e40cd6b61881e816cb2f9f449201
henrysmile2071 <henrychen.chc@gmail.com>
2022-11-18 22:30:55 +0800

    feat: editpage, newpage, show user error message, facebook authentication

---
+++ routes/modules/user.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const router = express.Router()
+
+const bcrypt = require('bcryptjs')
+const db = require('../../models')
+const User = db.User
+const passport = require('passport')
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: 'Please fill in all the fields.' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: 'password and confirm password do not match!' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      errors.push({ message: 'User already exists' })
+      return res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
+})
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  req.flash('success_msg', 'You have successfully logged out.')
+  res.redirect('/users/login')
+})
+
+module.exports = router
